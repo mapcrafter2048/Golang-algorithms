67cd7246eb6e2d98125bc6f8f0a77160a4f887b7
mapcrafter2048 <121042282+mapcrafter2048@users.noreply.github.com>
2024-04-09 02:28:18 +0530

    Added implementation of Linked list

--- linked-list.go
+++ linked-list.go
@@ -1,1 +1,43 @@
 package main
+
+import "fmt"
+
+type Node struct {
+	value int
+	next  *Node
+}
+
+type LinkedList struct {
+	head *Node
+}
+
+func (linkedList *LinkedList) Insert(data int) {
+	newNode := &Node{value: data, next: nil}
+
+	if linkedList.head == nil {
+		linkedList.head = newNode
+	} else {
+		current := linkedList.head
+		for current.next != nil {
+			current = current.next
+		}
+		current.next = newNode
+	}
+}
+
+func (linkedList *LinkedList) Display() {
+	current := linkedList.head
+	for current != nil {
+		fmt.Printf("%d ", current.value)
+		current = current.next
+	}
+	fmt.Println()
+}
+
+func main() {
+	linkedList := LinkedList{}
+	linkedList.Insert(1)
+	linkedList.Insert(2)
+	linkedList.Insert(3)
+	linkedList.Display()
+}
